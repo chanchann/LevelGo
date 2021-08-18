206a314d367ba9239c8c5324eec693efd2c0730d
chanchann <www.769440972@qq.com>
2021-08-18 11:50:05 +0800

    memtable

--- memtable/format.go
+++ memtable/format.go
@@ -1,2 +1,48 @@
 package memtable
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type ValueType int
+
+const (
+	TypeDeletion ValueType = 0
+	TypeValue ValueType = 1
+)
+
+type InternalKey struct {
+	rep []byte
+}
+
+func newInternalKey(seq int64, valueType ValueType, key, value []byte) *InternalKey {
+	// Format of an entry is concatenation of :
+	// 4 : key.size() + 8
+	// key bytes : char[key.size()]
+	// 8 : seq << 8 | valueType
+	// 4 : value.size()
+	// value bytes : char[value.size()]
+	internalKeySize := len(key) + 8
+	valueSize := len(value)
+	encodeLen := 4 + internalKeySize + 4 + valueSize 
+	buf := make([]byte, encodeLen)
+
+	offset := 0 
+	binary.LittleEndian.PutUint32(buf[offset:], uint32(internalKeySize))
+	offset += 4
+	copy(buf[offset:], key)
+	offset += len(key)
+	binary.LittleEndian.PutUint64(buf[offset:], (uint64(seq) << 8) | uint64(valueType))
+	offset += 8
+	binary.LittleEndian.PutUint32(buf[offset:], uint32(valueSize))
+	offset += 4
+	copy(buf[offset:], value)
+	
+	return &InternalKey{rep : buf}
+}
+
+func (internalKey *InternalKey) userKey() []byte {
+	
+}
+
--- memtable/memtable.go
+++ memtable/memtable.go
@@ -20,9 +20,29 @@ func (memTable *MemTable) NewIterator() levelgo.Iterator {   // todo
 	return memTable.table.NewIterator() 
 }
 
+// 将四个参数编码成一个internalKey
 func (memTable *MemTable) Add(seq int64, valueType ValueType, key, value []byte) {
 	internalKey := newInternalKey(seq, valueType, key, value)
 	memTable.table.Insert(internalKey)
+}
+
+func (memTable *MemTbale) Get(key []bytes) (bool, []byte, error) {
+	lookupKey := LookupKey(key)
+
+	it := memTbale.table.NewIterator()
+	it.Seek(lookupKey)
+	if it.Valid() {
+		internalKey := it.Key().(*InternalKey)
+		if UserKeyComparator(key, internalKey.userKey()) == 0 {
+			// 判断valueType
+			if internalKey.valueType() == TypeValue {
+				return true, internalKey.userValue(), nil
+			} else {
+				return true, nil, erros.New("not found")
+			}
+		}
+	}
+	return false, nil, errors.New("not found")
 }
 
 
